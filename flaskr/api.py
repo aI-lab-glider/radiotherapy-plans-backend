4c1b0d7ac96460c29736de806fc0b5befab87fba
Robert Koziarski <rob.koziarski2k@gmail.com>
2021-09-14 12:33:49 +0200

    saving an archive uploaded via api request & saving the contents after unzipping that archive - works fine

--- flaskr/api.py
+++ flaskr/api.py
@@ -2,8 +2,11 @@ from flask_restful import Resource, reqparse
 import os
 import werkzeug
 import requests
+import zipfile
 
 UPLOAD_DIR = '../static/uploads'
+DICOM_PATH_RELATIVE = UPLOAD_DIR + '/dicoms'
+DICOM_PATH_ABSOLUTE = '~/ProjectSummer/radiotherapy-plans-backend/static/uploads/dicoms'
 GENIE_API = 'http://127.0.0.1:8001/'
 
 class HelloWorld(Resource):
@@ -16,48 +19,31 @@ class FileUploads(Resource):
 
     def get(self):
         # get all files in the ./static/uploads directory
-        files = [f for f in os.listdir(UPLOAD_DIR)]
+        files = [f for f in os.listdir(UPLOAD_DIR + '/dicoms')]
         return {
-                'dir' : UPLOAD_DIR, 
+                'pathRelative': DICOM_PATH_RELATIVE,
+                'pathAbsolute': DICOM_PATH_ABSOLUTE,
                 'count' : len(files),
                 'files' : files,
                 }
 
     def post(self):
-        self.parser.add_argument('startComputation', required=True, type=str)
-        self.parser.add_argument('CTFile', required=False, \
-                type=werkzeug.datastructures.FileStorage, location='files')
-        self.parser.add_arguemnt('DoseDataFile', required=False, \
-                type=werkzeug.datastructures.FileStorage, location='files')
-        self.parser.add_argument('RSFile', required=False, \
+        self.parser.add_argument('dicomArchive', required=True, \
                 type=werkzeug.datastructures.FileStorage, location='files')
 
         args = self.parser.parse_args()
 
-        if 'CTFile' in args:
-            save_file(args.get('CTFile'), 'CT.zip')
-        if 'DoseSumFile' in args:
-            save_file(args.get('DoseSumFile'), 'DoseSum.zip')
-        if 'RSFile' in args:
-            save_file(args.get('RSFile'), 'RS.zip')
+        # save the .zip archive
+        archive = args.get('dicomArchive')
+        archive.save(os.path.join(UPLOAD_DIR, archive.filename))
+
+        # unzip the archive
+        with zipfile.ZipFile(os.path.join(UPLOAD_DIR, archive.filename), 'r') as zip_ref:
+            zip_ref.extractall(os.path.join(UPLOAD_DIR, 'dicoms'))
         
-        if args['startComputation']:
-            payload = {
-                    'CT_fname': 'CT.zip',
-                    'DoseSum_fname': 'DoseSum.zip',
-                    'RS_fname': 'RS.zip'
-                    }
-            response = requests.request('GET', GENIE_API+'load', headers={}, data=payload, files=[])
-            return {
-                    'successful': True,
-                    'computationStarted': True,
-                }
-
-        return {
-                'successful': True,
-                'computationStarted': False,
+        return { 
+                'message': 'files saved to path',
+                'pathRelative': DICOM_PATH_RELATIVE,
+                'pathAbsolute': DICOM_PATH_ABSOLUTE,
             }
-
-def save_file(file, filename):
-    file.save(os.path.join(UPLOAD_DIR, filename))
 
