123f7033b1c93329994551286d3bf37fcf2b02f6
Robert Koziarski <rob.koziarski2k@gmail.com>
2021-09-15 11:42:18 +0200

    changed workflow to python 3.9, made source coude changes using pylint

--- flaskr/api.py
+++ flaskr/api.py
@@ -1,8 +1,9 @@
-from flask_restful import Resource, reqparse
+"""API for the backend"""
+
 import os
-import werkzeug
-import requests
 import zipfile
+from flask_restful import Resource, reqparse
+import werkzeug
 
 import dicomutils
 
@@ -11,10 +12,6 @@ DICOM_PATH_RELATIVE = UPLOAD_DIR + '/dicoms'
 DICOM_PATH_ABSOLUTE = '~/ProjectSummer/radiotherapy-plans-backend/static/uploads/dicoms'
 GENIE_API = 'http://127.0.0.1:8001/'
 
-class HelloWorld(Resource):
-    def get(self):
-        return { 'hello': 'world' }
-
 class FileUploads(Resource):
 
     def __init__(self):
@@ -22,7 +19,7 @@ class FileUploads(Resource):
 
     def get(self):
         # get all files in the ./static/uploads directory
-        files = [f for f in os.listdir(UPLOAD_DIR + '/dicoms')]
+        files = list(os.listdir(UPLOAD_DIR + '/dicoms'))
         return {
                 'pathRelative': DICOM_PATH_RELATIVE,
                 'pathAbsolute': DICOM_PATH_ABSOLUTE,
@@ -38,24 +35,25 @@ class FileUploads(Resource):
 
         # save the .zip archive
         archive = args.get('dicomArchive')
-        archive.save(os.path.join(UPLOAD_DIR, archive.filename))
+        path_to_archive = os.path.join(UPLOAD_DIR, archive.filename)
+        archive.save(path_to_archive)
 
         # unzip the archive
-        with zipfile.ZipFile(os.path.join(UPLOAD_DIR, archive.filename), 'r') as zip_ref, err:
+        with zipfile.ZipFile(path_to_archive, 'r') as (zip_ref, err):
             if err == zipfile.BadZipFile:
                 handle_bad_zip_file(err)
             elif err == zipfile.LargeZipFile:
                 handle_large_zip_file(err)
             else:
-               zip_ref.extractall(os.path.join(UPLOAD_DIR, 'dicoms'))
+                zip_ref.extractall(os.path.join(UPLOAD_DIR, 'dicoms'))
 
-        # verify that the archive contains dicom files   
+        # verify that the archive contains dicom files
         try:
             check_archive_contents(path_to_archive)
         except dicomutils.InvalidDicomName as err:
             handle_invalid_contents(err)
 
-        return { 
+        return {
                 'status': 'success',
                 'message': 'files saved to path',
                 'pathRelative': DICOM_PATH_RELATIVE,
@@ -88,4 +86,3 @@ def handle_invalid_contents(ex: dicomutils.InvalidDicomName):
             'message': 'invalid name found inside the archive: {}'.format(ex.filename),
             'exceptionMessage': ex.message,
             }
-
--- flaskr/app.py
+++ flaskr/app.py
@@ -1,24 +1,22 @@
+"""Backend main module"""
+
 from flask import Flask
 from flask_restful import Api
 
-from api import HelloWorld, FileUploads
+from api import FileUploads
 
 # Flask application initialization
 app = Flask(__name__)
 
 # REST API initialization
 api = Api(app)
-hosted_api_root = "/api/" 
+HOSTED_API_ROOT = "/api/"
 
-api.add_resource(HelloWorld, hosted_api_root + 'hello')
-api.add_resource(FileUploads, hosted_api_root + 'upload')
-
-computation_api_url_base = "http://127.0.0.1:8001"
+api.add_resource(FileUploads, HOSTED_API_ROOT + 'upload')
 
 @app.route("/")
 def hello_world():
-   return "<p>Hello World!</p>"
+    return "<p>Hello World!</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
-
