15c16bab7c8b9f7683cf6865746331aabf78c2ab
Bohdan <qwebeck5@gmail.com>
2021-11-10 00:15:04 +0100

    Added initial api draft

--- computation/src/computation.jl
+++ computation/src/computation.jl
@@ -18,7 +18,7 @@ using Luxor
 
 
 function get_transform_matrix(dcm)
-    M = zeros(3,3)
+    M = zeros(3, 3)
     M[2,1] = dcm.ImageOrientationPatient[2] * dcm.PixelSpacing[1]
     M[1,1] = dcm.ImageOrientationPatient[1] * dcm.PixelSpacing[1]
     M[2,2] = dcm.ImageOrientationPatient[5] * dcm.PixelSpacing[2]
@@ -40,13 +40,13 @@ function transform_doses(dcm, ct_files)
     println("size(doses): ", size(doses))
     img_rescaled = imresize(doses, new_size)
     out = zeros(size(dcm_ct.PixelData)..., N)
-    shift_1 = -trunc(Int, dcm_ct.ImagePositionPatient[2]/dcm_ct.PixelSpacing[2] - dcm.ImagePositionPatient[2] / dcm_ct.PixelSpacing[2])
-    shift_2 = -trunc(Int, dcm_ct.ImagePositionPatient[1]/dcm_ct.PixelSpacing[1] - dcm.ImagePositionPatient[1] / dcm_ct.PixelSpacing[1])
-    #shift_1 = -trunc(Int, dcm.ImagePositionPatient[2] / dcm.PixelSpacing[2])+90
-    #shift_2 = -trunc(Int, dcm.ImagePositionPatient[1] / dcm.PixelSpacing[1])
+    shift_1 = -trunc(Int, dcm_ct.ImagePositionPatient[2] / dcm_ct.PixelSpacing[2] - dcm.ImagePositionPatient[2] / dcm_ct.PixelSpacing[2])
+    shift_2 = -trunc(Int, dcm_ct.ImagePositionPatient[1] / dcm_ct.PixelSpacing[1] - dcm.ImagePositionPatient[1] / dcm_ct.PixelSpacing[1])
+    # shift_1 = -trunc(Int, dcm.ImagePositionPatient[2] / dcm.PixelSpacing[2])+90
+    # shift_2 = -trunc(Int, dcm.ImagePositionPatient[1] / dcm.PixelSpacing[1])
     for i in 1:N
         copyto!(
-            view(out, shift_1:(shift_1+new_size[1]-1), shift_2:(shift_2+new_size[2]-1), N-i+1),
+            view(out, shift_1:(shift_1 + new_size[1] - 1), shift_2:(shift_2 + new_size[2] - 1), N - i + 1),
             img_rescaled[:, :, i],
         )
     end
@@ -61,7 +61,7 @@ function load_dicom(dir)
     for (idx, series) in enumerate(unique_series)
         dcms_in_series = filter(dcm -> dcm.SeriesInstanceUID == series, dcms)
         pixeldata = extract_pixeldata(dcms_in_series)
-        loaded_dcms[idx] = (; pixeldata = pixeldata, dcms = dcms_in_series)
+        loaded_dcms[idx] = (; pixeldata=pixeldata, dcms=dcms_in_series)
     end
     return loaded_dcms
 end
@@ -70,7 +70,7 @@ function extract_pixeldata(dcm_array)
     if length(dcm_array) == 1
         return only(dcm_array).PixelData
     else
-        return cat([dcm.PixelData for dcm in dcm_array]...; dims = 3)
+        return cat([dcm.PixelData for dcm in dcm_array]...; dims=3)
     end
 end
 
@@ -79,7 +79,7 @@ function combine_ct_doses(ct_px, doses, masks, primo_doses)
     out = RGB.(ct_px ./ maximum(ct_px))
     md = maximum(doses)
     for i in eachindex(out)
-        out[i] = out[i] + RGB(doses[i] / md, masks[i]/3, primo_doses[i]/md)
+        out[i] = out[i] + RGB(doses[i] / md, masks[i] / 3, primo_doses[i] / md)
     end
     return out
 end
@@ -99,7 +99,7 @@ function find_matching_seqs(sop_uid, dcm_rs, ROIname)
         if ssr.ROIName != ROIname
             continue
         end
-        #println(ssr)
+        # println(ssr)
         ROIs = [rcs for rcs in dcm_rs.ROIContourSequence if rcs.ReferencedROINumber == ssr.ROINumber]
         ROI = ROIs[1]
         if ROI.ContourSequence !== nothing
@@ -107,7 +107,7 @@ function find_matching_seqs(sop_uid, dcm_rs, ROIname)
                 alleq = true
                 if seq.ContourImageSequence !== nothing
                     for cis in seq.ContourImageSequence
-                        #println(cis.ReferencedSOPInstanceUID)
+                        # println(cis.ReferencedSOPInstanceUID)
                         if cis.ReferencedSOPInstanceUID != sop_uid
                             alleq = false
                             break
@@ -159,11 +159,11 @@ function extract_roi_masks(dcm_ct, dcm_rs)
 end
 
 function make_normals(doses, algo, origin, widths)
-    mc = GeometryBasics.Mesh(doses, algo; origin = origin, widths = widths)
+    mc = GeometryBasics.Mesh(doses, algo; origin=origin, widths=widths)
     itp = Interpolations.scale(interpolate(doses, BSpline(Quadratic(Periodic(OnGrid())))),
-            range(origin[1], origin[1] + widths[1], length=size(doses,1)),
-            range(origin[2], origin[2] + widths[2], length=size(doses,2)),
-            range(origin[3], origin[3] + widths[3], length=size(doses,3)))
+            range(origin[1], origin[1] + widths[1], length=size(doses, 1)),
+            range(origin[2], origin[2] + widths[2], length=size(doses, 2)),
+            range(origin[3], origin[3] + widths[3], length=size(doses, 3)))
     normals = [normalize(Vec3f0(Interpolations.gradient(itp, Tuple(v)...))) for v in mc.position]
 
     new_mesh = GeometryBasics.Mesh(GeometryBasics.meta(mc.position; normals=normals), faces(mc))
@@ -189,9 +189,9 @@ function ct_origin_widths(ct_files)
     dcm_sample_ct = ct_files[1].dcms[1]
     origin = SA[0.0, 0.0, 0.0]
     widths = SA[
-        Float32(dcm_sample_ct.Rows*dcm_sample_ct.PixelSpacing[1]),
-        Float32(dcm_sample_ct.Columns*dcm_sample_ct.PixelSpacing[2]),
-        Float32(get_slice_thickness(ct_files)*length(ct_files[1].dcms)),
+        Float32(dcm_sample_ct.Rows * dcm_sample_ct.PixelSpacing[1]),
+        Float32(dcm_sample_ct.Columns * dcm_sample_ct.PixelSpacing[2]),
+        Float32(get_slice_thickness(ct_files) * length(ct_files[1].dcms)),
     ]
     return origin, widths
 end
@@ -221,23 +221,23 @@ function make_CT_mesh(ct_files; isolevel::Float64=1200.0, body_mask=nothing)
     return ct_mesh
 end
 
-function make_mesh(doses, ct_files, roi_masks, rois_to_plot = [];
-    dose_discrepancy = nothing,
-    primo_doses = nothing,
-    trim_ct_to_body = true,
-    trim_doses_to_body = true,
-    trim_doses_to_rois = false,
-    level_max = 60.0,
-    tps_isodose_levels = range(5.0, level_max, length = 5),
-    primo_isodose_levels = range(5.0, level_max, length = 5),
-    palette_tps = ColorSchemes.isoluminant_cgo_70_c39_n256,
-    palette_primo = ColorSchemes.isoluminant_cgo_70_c39_n256,
+function make_mesh(doses, ct_files, roi_masks, rois_to_plot=[];
+    dose_discrepancy=nothing,
+    primo_doses=nothing,
+    trim_ct_to_body=true,
+    trim_doses_to_body=true,
+    trim_doses_to_rois=false,
+    level_max=60.0,
+    tps_isodose_levels=range(5.0, level_max, length=5),
+    primo_isodose_levels=range(5.0, level_max, length=5),
+    palette_tps=ColorSchemes.isoluminant_cgo_70_c39_n256,
+    palette_primo=ColorSchemes.isoluminant_cgo_70_c39_n256,
     tps_isodose_alpha=0.1f0,
     primo_isodose_alpha=0.1f0,
     bone_alpha=0.1f0,
-    hot_cold_level = nothing,
+    hot_cold_level=nothing,
 )
-    #Makie.scatter([0.0, 1.0], [0.0, 1.0], [0.0, 1.0])
+    # Makie.scatter([0.0, 1.0], [0.0, 1.0], [0.0, 1.0])
     scene = Makie.Scene()
 
     body_mask = haskey(roi_masks, "BODY") ? roi_masks["BODY"] : one.(first(roi_masks)[2])
@@ -258,22 +258,22 @@ function make_mesh(doses, ct_files, roi_masks, rois_to_plot = [];
     end
     # show something from CT
     begin
-        ct_mesh = make_CT_mesh(ct_files; body_mask = trim_ct_to_body ? body_mask : nothing)
+        ct_mesh = make_CT_mesh(ct_files; body_mask=trim_ct_to_body ? body_mask : nothing)
         Makie.mesh!(
             scene,
             ct_mesh,
             color=RGBA{Float32}(1.0f0, 1.0f0, 1.0f0, bone_alpha),
-            ssao = false,
-            transparency = true,
-            shininess = 400.0f0,
-            lightposition = Makie.Vec3f0(200, 200, 500),
+            ssao=false,
+            transparency=true,
+            shininess=400.0f0,
+            lightposition=Makie.Vec3f0(200, 200, 500),
              # base light of the plot only illuminates red colors
-            ambient = Vec3f0(0.3, 0.3, 0.3),
+            ambient=Vec3f0(0.3, 0.3, 0.3),
             # light from source (sphere) illuminates yellow colors
-            diffuse = Vec3f0(0.4, 0.4, 0.4),
+            diffuse=Vec3f0(0.4, 0.4, 0.4),
             # reflections illuminate blue colors
-            specular = Vec3f0(1.0, 1.0, 1.0),
-            show_axis = false,
+            specular=Vec3f0(1.0, 1.0, 1.0),
+            show_axis=false,
         )
     end
   
@@ -349,10 +349,10 @@ Given `kwargs` are passed to `make_CT_mesh`.
 
 `ct_mesh_from_files(dd, "/tmp/test.obj"; isolevel=1000.0)`
 """
-function ct_mesh_from_files(dd::DoseData, ct_mesh_fname; kwargs...)
+function create_mesh_and_save(dd::DoseData, save_to; kwargs...)
     ct_mesh = make_CT_mesh(dd.ct_files; kwargs...)
-    save(ct_mesh_fname, ct_mesh)
-    return ct_mesh_fname
+    save(save_to, ct_mesh)
+    return save_to
 end
 
 """
@@ -368,7 +368,7 @@ Given `kwargs` are passed to `make_CT_mesh`.
 
 `ct_mesh_from_files(dd, "/tmp/test.obj"; isolevel=1000.0)`
 """
-function ct_mesh_from_files(dd::DoseData, ct_mesh_fname; kwargs...)
+function create_mesh_and_save(dd::DoseData, ct_mesh_fname; kwargs...)
     ct_mesh = make_CT_mesh(dd.ct_files; kwargs...)
     save(ct_mesh_fname, ct_mesh)
     return ct_mesh_fname
@@ -389,7 +389,7 @@ function make_ROI_mesh(dd::DoseData, roi_name, roi_mesh_fname)
 end
 
 
-"""
+    """
     test_scene()
 
 Display the `hnscc_7` scene using Makie.jl (for testing purposes).
@@ -403,14 +403,14 @@ function test_scene()
         selected_data.roi_masks,
         highlight;
         primo_doses=selected_data.primo_filtered_in_Gy,
-        tps_isodose_levels = [],
-        primo_isodose_levels = [],
-        #palette_primo = ColorSchemes.linear_kry_5_95_c72_n256,
-        level_max = 65.0,
-        #palette_tps= ColorSchemes.RdBu_11,
-        #tps_isodose_alpha=0.5,
-        #primo_isodose_alpha=0.2,
-        trim_doses_to_rois = true,
+        tps_isodose_levels=[],
+        primo_isodose_levels=[],
+        # palette_primo = ColorSchemes.linear_kry_5_95_c72_n256,
+        level_max=65.0,
+        # palette_tps= ColorSchemes.RdBu_11,
+# tps_isodose_alpha=0.5,
+        # primo_isodose_alpha=0.2,
+        trim_doses_to_rois=true,
         hot_cold_level=63.0,
     )
 end
