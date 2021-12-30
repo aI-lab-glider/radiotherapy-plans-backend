757995659630ed91814508d67a2a910a71af8c1e
Mateusz Baran <mateuszbaran89@gmail.com>
2021-12-30 11:25:30 +0100

    cleanup of Julia computation

--- computation/src/computation.jl
+++ computation/src/computation.jl
@@ -18,19 +18,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-using Images, ImageView, DICOM
-using Plots
+using DICOM
 using Interpolations
 using ImageFiltering
 
-using Makie
 using LinearAlgebra
 using Meshing
 using MeshIO
 using GeometryBasics
 using StaticArrays
-using GLMakie
-GLMakie.enable_SSAO[] = false
 using ColorSchemes
 using Statistics
 using Luxor
@@ -311,65 +307,6 @@ function make_CT_mesh(ct_files; isolevel::Float64 = 1200.0, body_mask = nothing)
     return ct_mesh
 end
 
-function make_mesh(doses, ct_files, roi_masks, rois_to_plot = [];
-    dose_discrepancy = nothing,
-    primo_doses = nothing,
-    trim_ct_to_body = true,
-    trim_doses_to_body = true,
-    trim_doses_to_rois = false,
-    level_max = 60.0,
-    tps_isodose_levels = range(5.0, level_max, length = 5),
-    primo_isodose_levels = range(5.0, level_max, length = 5),
-    palette_tps = ColorSchemes.isoluminant_cgo_70_c39_n256,
-    palette_primo = ColorSchemes.isoluminant_cgo_70_c39_n256,
-    tps_isodose_alpha = 0.1f0,
-    primo_isodose_alpha = 0.1f0,
-    bone_alpha = 0.1f0,
-    hot_cold_level = nothing
-)
-    # Makie.scatter([0.0, 1.0], [0.0, 1.0], [0.0, 1.0])
-    scene = Makie.Scene()
-
-    body_mask = haskey(roi_masks, "BODY") ? roi_masks["BODY"] : one.(first(roi_masks)[2])
-
-    function trim_doses(input_doses)
-        trimmed_doses = copy(input_doses)
-        if trim_doses_to_body
-            trimmed_doses[(!).(body_mask)] .= false
-        end
-        if trim_doses_to_rois
-            roi_or = fill(false, size(body_mask)...)
-            for (roi_name, roi_color) in rois_to_plot
-                roi_or .|= roi_masks[roi_name]
-            end
-            trimmed_doses[(!).(roi_or)] .= false
-        end
-        return trimmed_doses
-    end
-    # show something from CT
-    begin
-        ct_mesh = make_CT_mesh(ct_files; body_mask = trim_ct_to_body ? body_mask : nothing)
-        Makie.mesh!(
-            scene,
-            ct_mesh,
-            color = RGBA{Float32}(1.0f0, 1.0f0, 1.0f0, bone_alpha),
-            ssao = false,
-            transparency = true,
-            shininess = 400.0f0,
-            lightposition = Makie.Vec3f0(200, 200, 500),
-            # base light of the plot only illuminates red colors
-            ambient = Vec3f0(0.3, 0.3, 0.3),
-            # light from source (sphere) illuminates yellow colors
-            diffuse = Vec3f0(0.4, 0.4, 0.4),
-            # reflections illuminate blue colors
-            specular = Vec3f0(1.0, 1.0, 1.0),
-            show_axis = false,
-        )
-    end
-
-    return scene
-end
-
 """
     read_f0(fname)
 
@@ -400,13 +337,20 @@ function read_f0(fname)
             for x in 1:img_size[1]
                 rl = readline(file)
                 curline = parse.(Float64, split(rl))
-                doses[z, x, img_size[2]-y+1] = curline[1]
-                two_sigmas[z, x, img_size[2]-y+1] = curline[2]
+                doses[z, x, y] = curline[1]
+                two_sigmas[z, x, y] = curline[2]
             end
         end
     end
+
+    if all(z_pos_diffs(ct_files) .> 0)
+        doses .= reverse(doses; dims=3)
+        two_sigmas .= reverse(two_sigmas; dims=3)
+    end
+
     close(file)
-    even_newer_size = (512, 512, new_size[3])
+    dcm_ct = ct_files[1].dcms[1]
+    even_newer_size = (Int(dcm_ct.Rows), Int(dcm_ct.Columns), new_size[3])
     doses_rescaled = imresize(doses, even_newer_size)
     two_sigmas_rescaled = imresize(two_sigmas, even_newer_size)
     return doses_rescaled, two_sigmas_rescaled
@@ -441,6 +385,19 @@ function calc_dvh_for_doses(q, doses)
     return 1.0 .- f.(q)
 end
 
+"""
+    DoseData
+
+Loaded DICOM files for one RT patient, including CT, planned doses, ROI masks and delivered
+doses.
+"""
+struct DoseData{TCT,TPD,TRM,TDD}
+    ct_files::TCT
+    doses::TPD
+    roi_masks::TRM
+    primo_filtered_in_Gy::TDD
+end
+
 
 """
     calc_plots_data(dd::DoseData; N=1000)
@@ -450,13 +407,20 @@ for each isodose level from 0 to maximum of planned doses, at `N` levels.
 """
 function calc_plots_data(dd::DoseData; N=1000)
 
-    md = maximum(dd.doses)
+    roi_to_plotdata = Dict{String,NamedTuple}()
+    boolmask = convert(Array{Bool}, mask)
+    md = NaN
+    try
+        # can fail for empty ROIs
+        md = max(maximum(dd.doses[boolmask]), maximum(dd.primo_filtered_in_Gy[boolmask]))
+    catch e
+        return ([], roi_to_plotdata)
+    end
     q = range(0.0, md; length=N)
 
     ct_1 = dd.ct_files[1].dcms[1]
     hausdorff_weights = (ct_1.PixelSpacing..., ct_1.SliceThickness)
 
-    roi_to_plotdata = Dict{String,NamedTuple}()
     for (roi_name, roi_mask) in dd.roi_masks
 
         fprs = zeros(N)
@@ -491,19 +455,6 @@ end
 # RTSTRUCT: structures in the CT scan
 
 """
-    DoseData
-
-Loaded DICOM files for one RT patient, including CT, planned doses, ROI masks and delivered
-doses.
-"""
-struct DoseData{TCT,TPD,TRM,TDD}
-    ct_files::TCT
-    doses::TPD
-    roi_masks::TRM
-    primo_filtered_in_Gy::TDD
-end
-
-"""
     load_DICOMs(CT_fname, dose_sum_fname, rs_fname)
 
 Load given DICOM files.
@@ -526,20 +477,6 @@ function load_DICOMs(CT_fname, dose_sum_fname, rs_fname)
 
     return DoseData(ct_files, doses, roi_masks, primo_filtered_in_Gy)
 end
-
-"""
-    HNSCC_BASE_PATH
-
-Base path to HNSCC data files. They can be downloaded using the provided manifest file.
-"""
-# const HNSCC_BASE_PATH = "test-data/HNSCC/HNSCC/"
-
-# ### loading a sample file from the NBIA dataset
-# hnscc_7 = load_DICOMs(
-#     HNSCC_BASE_PATH * "HNSCC-01-0007/04-29-1997-RT SIMULATION-32176/10.000000-72029/",
-#     HNSCC_BASE_PATH * "HNSCC-01-0007/04-29-1997-RT SIMULATION-32176/1.000000-09274/1-1.dcm",
-#     HNSCC_BASE_PATH * "HNSCC-01-0007/04-29-1997-RT SIMULATION-32176/1.000000-06686/1-1.dcm",
-# )
 
 """
     ct_mesh_from_files(dd::DoseData, ct_mesh_fname; kwargs...)
@@ -574,29 +511,3 @@ function make_ROI_mesh(dd::DoseData, roi_name, roi_mesh_fname)
     return roi_mesh_fname
 end
 
-
-"""
-    test_scene()
-
-Display the `hnscc_7` scene using Makie.jl (for testing purposes).
-"""
-function test_scene()
-    selected_data = hnscc_7
-    highlight = [("PTV", RGBA{Float32}(0.0f0, 1.0f0, 0.0f0, 0.1f0))]
-    scene = make_mesh(
-        selected_data.doses,
-        selected_data.ct_files,
-        selected_data.roi_masks,
-        highlight;
-        primo_doses = selected_data.primo_filtered_in_Gy,
-        tps_isodose_levels = [],
-        primo_isodose_levels = [],
-        # palette_primo = ColorSchemes.linear_kry_5_95_c72_n256,
-        level_max = 65.0,
-        # palette_tps= ColorSchemes.RdBu_11,
-        # tps_isodose_alpha=0.5,
-        # primo_isodose_alpha=0.2,
-        trim_doses_to_rois = true,
-        hot_cold_level = 63.0
-    )
-end
